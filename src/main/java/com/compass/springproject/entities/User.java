4a6d89505052dd3e38985bbbeda93e2a3fd85f2e
GustavoBeckSilva <gustavogbks@gmail.com>
2025-03-19 17:34:28 -0300

    Creating the Order class

--- src/main/java/com/compass/springproject/entities/User.java
+++ src/main/java/com/compass/springproject/entities/User.java
@@ -1,17 +1,20 @@
 package com.compass.springproject.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements Serializable{ // Relação um para muitos com pedido
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -22,11 +25,13 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+		
+	@OneToMany(mappedBy = "client") // Atributo mapeado pela classe Pedido
+	private List<Order> orders = new ArrayList<>(); // Não se criar setters em coleções
 	
 	public User() {}
 
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -73,6 +78,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	 public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,7 +99,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 	
 }
