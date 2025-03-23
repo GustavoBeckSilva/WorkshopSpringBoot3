1828979c9d822785711b9ce7ce11a3f3cf5f44f2
GustavoBeckSilva <gustavogbks@gmail.com>
2025-03-23 16:13:09 -0300

    Creating the delete exception

--- src/main/java/com/compass/springproject/services/UserService.java
+++ src/main/java/com/compass/springproject/services/UserService.java
@@ -3,9 +3,12 @@ package com.compass.springproject.services;
 import java.util.List;
 import java.util.Optional;
 import com.compass.springproject.resources.exceptions.ResourceExceptionHandler;
+import com.compass.springproject.services.exceptions.DatabaseException;
 import com.compass.springproject.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.compass.springproject.entities.User;
@@ -39,7 +42,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());// Lançada exceção personalizada para violação de integridade de dados
+		}
 	}
 	
 	public User update(Long id, User obj) {
