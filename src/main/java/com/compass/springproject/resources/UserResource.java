46812cdeacf19f7eb4a7196e5300a84e7a0776ee
GustavoBeckSilva <gustavogbks@gmail.com>
2025-03-23 15:13:53 -0300

    Creating the User Insert operation

--- src/main/java/com/compass/springproject/resources/UserResource.java
+++ src/main/java/com/compass/springproject/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.compass.springproject.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.compass.springproject.entities.User;
 import com.compass.springproject.services.UserService;
@@ -31,6 +35,18 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){ // Marcação usada para indicar que o parâmetro veio por URL
 		User user = userService.FindById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	// Para recuperar dados, usa-se o get. Para inserir dados, usa-se o post
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){ // A marcação indica que o objeto chega em formato JSON e é desserializado para o objeto User
+		
+		obj = userService.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		// É correto retornar o código 201, indicando a criação de recurso
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
